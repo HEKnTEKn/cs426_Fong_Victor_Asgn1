3bdeeb60db8b96e9ed2e687e21b5cca816969529
Victor Fong <vfong3@uic.edu>
2021-01-27 00:38:57 -0600

    Initial commit

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+	public Score scoreManager;
+
+	//this method is called whenever a collision is detected
+	private void OnCollisionEnter(Collision collision)
+	{
+
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			//on collision adding point to the score
+			scoreManager.AddPoint();
+			// printing if collision is detected on the console
+			Debug.Log("Proper Collision Detected");
+			//after collision is detected destroy the gameobject
+			Destroy(gameObject);
+		}
+		else
+		{
+			
+			Debug.Log("ImProper Collision Detected");
+
+		}
+		
+		
+	}
+}
